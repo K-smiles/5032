2eb238a0fbaa727fc36d54d07c9891a2f64b946d
K-smiles <lu452126871@gmail.com>
2023-08-29 10:45:58 +1000

    week6

---
+++ week6/FIT_5032MyViewModel/FIT_5032MyViewModel/Models/SampleFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace FIT_5032MyViewModel.Models
+{
+    public class SampleFormViewModel
+    {
+    }
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+
+}
---
+++ week6/FIT_5032MyViewModel/FIT_5032MyViewModel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT_5032MyViewModel.Startup))]
+namespace FIT_5032MyViewModel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
